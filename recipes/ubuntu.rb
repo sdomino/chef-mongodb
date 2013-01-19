ca2a32e480ad6755a3e41d62b212d139e1d853c8
Steve Domino <domino.steve@gmail.com>
2013-01-18 21:20:41 -0700

    master - NEW: trying to get ubuntu working

--- recipes/ubuntu.rb
+++ recipes/ubuntu.rb
@@ -2,7 +2,7 @@ case node['mongodb']['install_type']
 when 'package'
   execute 'sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10'
   cookbook_file '/etc/apt/sources.list.d/10gen.list'
-  include_recipe 'apt'
+  execute 'sudo apt-get update'
   package 'mongodb-10gen'
   service 'mongodb' do
     action [:enable, :start]
